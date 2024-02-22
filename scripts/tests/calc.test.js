4fac27d7cf4eb1a96da13a18f4af08f07b3713fd
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-02-22 12:26:50 +0000

    Add advanced DOM mock

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -8,7 +8,7 @@ describe("Calculator",() => {
         })
     });
         test("should return 73 for 42 + 31", () => {
-            expect(addition(42, -31)).toBe(73);
+            expect(addition(42, 31)).toBe(73);
     })
     describe("Subtraction function", () => {
     
