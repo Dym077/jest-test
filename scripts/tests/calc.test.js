7afb76a1460aed8f880de5291c99d2000f491bd1
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-02-22 10:51:17 +0000

    Modify addition function

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -6,9 +6,11 @@ describe("Calculator",() => {
             expect(addition(20, 22)).toBe(42);
         })
     });
-
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, -31)).toBe(73);
+    })
     describe("Subtraction function", () => {
-        
+    
     });
 
     describe("Multiply function", () => {
